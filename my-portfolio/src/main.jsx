24966a2ef3e2c42b7171a7b54712113961a5d038
w3dd1e <kelly.weddle@gmail.com>
2024-12-12 23:38:38 -0600

    Add responseive menu bar

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import './index.css';
+import App from './App.jsx';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</StrictMode>
+);
